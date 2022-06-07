686aefc90f89377c165dfd1626cbd6d726a036f8
robertocaixeta <robertocaixeta102@gmail.com>
2022-06-06 22:33:26 -0300

    :heavy_plus_sign: Adiciona o react navigate à aplicação

---
+++ switch-gym/src/router.js
@@ -0,0 +1,37 @@
+import react from "react";
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { Feather } from '@expo/vector-icons'
+
+import Cadastro from "./pages/Cadastro"
+import Edit from "./pages/Edit"
+import Index from "./pages/Index"
+import Login from "./pages/Login"
+import Show from "./pages/Show"
+
+const Stack = createStackNavigator()
+
+function Routes() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen 
+                name="cadastro" 
+                component={Cadastro}
+                options={{
+                    title:'Cadastre-se',
+                    headerTitleStyle: {
+                        fontFamily: 'Montserrat_700Bold'
+                    }   
+                }}
+                 />
+                <Stack.Screen name="edit" component={Edit} />
+                <Stack.Screen name="index" component={Index} />
+                <Stack.Screen name="login" component={Login} />
+                <Stack.Screen name="show" component={Show} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default Routes;
