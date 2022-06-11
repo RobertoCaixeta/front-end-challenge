04d97ffa0d552853b0e87da065db0d54c4c89757
robertocaixeta <robertocaixeta102@gmail.com>
2022-06-11 16:34:15 -0300

    :sparkles: Cria tela de login

--- switch-gym/src/pages/Login.js
+++ switch-gym/src/pages/Login.js
@@ -1,10 +1,89 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Button, Alert } from 'react-native';
+import api from '../services/api';
 
-export default function Login() {
- return (
-   <View>
-       <Text>Login</Text>
-   </View>
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+  const Login = () => {
+    api
+      .post("/users/login", {
+        user: {
+          email: email,
+          password: senha,
+        }
+
+      }
+      ).then((response) => {
+        AsyncStorage.setItem("ROLE", response.data.role)
+        AsyncStorage.setItem("TOKEN", response.headers.authorization)
+        navigation.navigate("index")
+      }).catch((error) => {
+        console.log("erro", error)
+        Alert.alert("Erro ao fazer login!")
+      })
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputTextArea}>
+        <Text style={styles.title} >Switch Gym</Text>
+        <Text style={styles.label} >Email</Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Email'
+          value={email}
+          onChangeText={setEmail}
+        />
+        <Text style={styles.label} >Senha</Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Senha'
+          value={senha}
+          onChangeText={setSenha}
+          secureTextEntry={true}
+        />
+
+        <Button
+          title='Entrar'
+          onPress={() => Login()}
+        />
+      </View>
+    </View>
   );
-}  
+
+
+}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#33ccff",
+    flex: 1,
+    alignItems: 'center',
+  },
+  input: {
+    backgroundColor: "#fff",
+    height: 40,
+    width: 200,
+    marginVertical: 15,
+    elevation: 2,
+    borderRadius: 10,
+    paddingLeft: 5
+
+  },
+  label: {
+    color: "#fff",
+    fontSize: 25
+  },
+  title: {
+    color: "#fff",
+    fontSize: 50
+  },
+  inputTextArea: {
+    alignItems: 'center',
+  },
+  text: {
+    color: "#fff",
+    fontSize: 15,
+    marginVertical: 10,
+  }
+})
